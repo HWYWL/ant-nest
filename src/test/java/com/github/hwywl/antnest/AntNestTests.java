7514b118a836451de5bf6ab7a5ab5cc1cbba5bc4
hwy <ilovey_hwy@163.com>
2019-05-23 16:43:06 +0800

    test

--- src/test/java/com/github/hwywl/antnest/AntNestTests.java
+++ src/test/java/com/github/hwywl/antnest/AntNestTests.java
@@ -1,11 +1,19 @@
 package com.github.hwywl.antnest;
 
+import com.github.hwywl.antnest.vo.User;
 import org.junit.Test;
 
 public class AntNestTests {
 
     @Test
     public void contextLoads() {
+        User user = new User();
+
+        user.setName("  sdsd\n" +
+                "xd\n" +
+                "dd\n" +
+                "ss\\rdd\\r\\n最后的");
+        System.out.println(user.getName());
     }
 
 }
